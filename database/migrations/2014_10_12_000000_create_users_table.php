2b1b66e7037f182d4984b00ecffbdbf7f1610960
Abderrahmane_Ahouari <abdorrahmane054@gmail.com>
2025-02-25 14:14:52 +0000

    created the user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone');
+            $table->enum('role',['driver','passenger'])->default('driver');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
@@ -29,4 +31,5 @@ return new class extends Migration
     {
         Schema::dropIfExists('users');
     }
+
 };
